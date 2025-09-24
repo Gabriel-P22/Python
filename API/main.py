4d0af0e4a82f6027e36b7ec039cd53ba664a1de8
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-09-24 10:04:50 -0300

    feature: add api package

---
+++ API/main.py
@@ -0,0 +1,27 @@
+import requests
+
+url = "https://guilhermeonrails.github.io/api-restaurantes/restaurantes.json"
+
+response = requests.get(url);
+print(response)
+res = response.json()
+
+if response.status_code == 200:    
+    restaurant_dto = {}
+
+    for item in res:
+        name = item["Company"]
+
+        if name not in restaurant_dto:
+            restaurant_dto[name] = []
+        
+        restaurant_dto[name].append({
+            "item": item["Item"],
+            "price": item["price"],
+            "description": item["description"]
+        })
+       
+
+    print(restaurant_dto["McDonald’s"])
+else:
+    print(f"O erro foi {response.status_code}")
