381cb853efaa9ece6b507b38dcbd1f00ffcd3c28
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-09-26 09:23:14 -0300

    feature: .env and CRUD

--- WEB_APP/WEB_APP_WITH_FLASK/app/__init__.py
+++ WEB_APP/WEB_APP_WITH_FLASK/app/__init__.py
@@ -1,7 +1,20 @@
 from flask import Flask
-from .routes.main import main__bp
+from pymongo import MongoClient
+
+db = None
 
 def create_app() -> Flask:
     app = Flask(__name__)
+    app.config.from_object('config.Config')
+    global db
+
+    try:
+        client = MongoClient(app.config['MONGO_URI'])
+        db = client.get_default_database()
+    except Exception as e:
+        print(f'Error on setup database config {e}')
+
+    from .routes.main import main__bp
     app.register_blueprint(main__bp)
+
     return app
--- WEB_APP/WEB_APP_WITH_FLASK/app/routes/main.py
+++ WEB_APP/WEB_APP_WITH_FLASK/app/routes/main.py
@@ -1,6 +1,88 @@
-from flask import Blueprint, jsonify, Response
+from flask import Blueprint, jsonify, Response, request
+from pydantic import ValidationError
+from WEB_APP.WEB_APP_WITH_FLASK.app import db
+from bson import ObjectId
+from WEB_APP.WEB_APP_WITH_FLASK.app.models.user import LoginPayload
+from WEB_APP.WEB_APP_WITH_FLASK.app.models.products import Product, ProductDBModel
 
 main__bp = Blueprint("main_bp", __name__)
+
+@main__bp.route('/login', methods=['POST'])
+def login():
+
+    try:
+        payload = request.get_json()
+        user_data = LoginPayload(**payload)
+        pass
+    except ValidationError as e:
+        return jsonify({
+            "error":
+                e.errors()
+        }), 400
+    except Exception as e:
+        return jsonify({
+            "error":
+                "error in login"
+        }), 500
+
+    if user_data.username == "admin" and user_data.password == "123":
+        return jsonify({
+            "message": f"Realizar o Login do adm {user_data.model_dump_json()}"
+        })
+    else:
+        return jsonify({
+            "message": f"Realizar o Login{user_data.model_dump_json()}"
+        })
+
+@main__bp.route('/products', methods=["POST"])
+def create_products() -> Response:
+    return jsonify({
+        "message":
+            "Create a product!"
+    })
+
+@main__bp.route('/product/<int:product_id>')
+def get_product_by_id(product_id) -> Response:
+    return jsonify({
+        "message":
+            "Product find!"
+    })
+
+
+@main__bp.route('/products')
+def get_products() -> Response:
+    products_cursor = db.products.find({})
+    products_list = []
+
+    for products in products_cursor:
+        products['_id'] = str(products['_id'])
+        products_list.append(products)
+
+    return jsonify({
+        "list":
+            products_list
+    })
+
+@main__bp.route('/product/<int:product_id>', methods=["PUT"])
+def update_product_by_id(product_id) -> Response:
+    return jsonify({
+        "message":
+            "Product find!"
+    })
+
+@main__bp.route('/product/<int:product_id>', methods=["DELETE"])
+def update_product_by_id(product_id) -> Response:
+    return jsonify({
+        "message":
+            "Product find!"
+    })
+
+@main__bp.route('/sales/upload', methods=["POST"])
+def upload_sales() -> Response:
+    return jsonify({
+        "message":
+            "Product find!"
+    })
 
 @main__bp.route('/')
 def index() -> Response:
@@ -9,16 +91,3 @@ def index() -> Response:
             "Welcome to StyleSync!"
     })
 
-@main__bp.route('/products')
-def get_products() -> Response:
-    return jsonify({
-        "message":
-            "Welcome to StyleSync products!"
-    })
-
-@main__bp.route('/login', methods=['POST'])
-def login() -> Response:
-    return jsonify({
-        "message":
-            "Welcome to StyleSync products!"
-    })
