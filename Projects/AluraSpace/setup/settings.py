a550f811d1e7724b70f25711cf1f03ae648767a8
Gabriel <paschoalsoftwareengineer@gmail.com>
2025-09-26 10:28:58 -0300

    feature: add env

--- Projects/AluraSpace/setup/settings.py
+++ Projects/AluraSpace/setup/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nkr=kvi#gi^t-nf-r&01xrdh*#%9)rfmwgx585cf_g54!lxw6h'
+SECRET_KEY = str(os.getenv('DJANGO_SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
